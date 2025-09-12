5fbdaf2c268c39059b6ec7fa5fcf5503d5c31aa8
Bernesh-stack <bernesh.in@gmail.com>
2025-09-12 21:17:48 +0530

    Complete of backend

--- streamchatbackend/src/serverClient.ts
+++ streamchatbackend/src/serverClient.ts
@@ -1,13 +1,12 @@
 import { StreamChat } from "stream-chat";
 
+export const apiKey = process.env.STREAM_API_KEY as string;
+export const apiSecret = process.env.STREAM_API_SECRET as string;
 
-export const apiKey = process.env.STREAM_API_KEY;
-export const apiSecret = process.env.STREAM_API_SECRET;
-
-if(!apiKey || !apiSecret) {
-    throw new Error("STREAM_API_KEY and STREAM_API_SECRET must be set");
+if (!apiKey || !apiSecret) {
+  throw new Error(
+    "Missing required environment variables STREAM_API_KEY or STREAM_API_SECRET"
+  );
 }
 
-
 export const serverClient = new StreamChat(apiKey, apiSecret);
- 
